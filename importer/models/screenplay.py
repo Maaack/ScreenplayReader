7a5ae476314d883246084481b696377bcaaab0c0
Maaack <marek.belski@gmail.com>
2018-02-06 13:14:45 -0800

    Interpreter: Purges the things

--- importer/models/screenplay.py
+++ importer/models/screenplay.py
@@ -3,21 +3,17 @@ from django.utils.translation import ugettext_lazy as _
 from .importer import BaseModel, RawText, RawTitle, models
 
 
-class Screenplay(BaseModel):
+class Screenplay(BaseModel, RawText):
     class Meta:
         verbose_name = _('Screenplay')
         verbose_name_plural = _('Screenplays')
         ordering = ["-created"]
         default_related_name = 'screenplays'
 
-    title_page = models.OneToOneField('TitlePage', models.SET_NULL, null=True)
-    content = models.OneToOneField('Content', models.CASCADE)
+    interpret_operation = models.ForeignKey('InterpretOperation', models.CASCADE)
 
     def __str__(self):
-        try:
-            return str(self.title_page)
-        except TitlePage.DoesNotExist:
-            return str(self.content)
+        return self.text[:25]
 
 
 class TitlePage(BaseModel, RawTitle, RawText):
@@ -26,39 +22,7 @@ class TitlePage(BaseModel, RawTitle, RawText):
         verbose_name_plural = _("TitlePages")
         ordering = ["-created"]
 
+    interpret_operation = models.ForeignKey('InterpretOperation', models.CASCADE)
+
     def __str__(self):
         return self.title
-
-
-class Content(BaseModel, RawText):
-    class Meta:
-        verbose_name = _("Content")
-        verbose_name_plural = _("Contents")
-        ordering = ["-created"]
-        default_related_name = 'contents'
-
-    def __str__(self):
-        return self.text[:25]
-
-
-class ContentElement(BaseModel, RawText):
-    class Meta:
-        verbose_name = _("Content Element")
-        verbose_name_plural = _("Content Elements")
-        ordering = ["-created"]
-        default_related_name = 'content_elements'
-
-    content = models.ForeignKey('Content', models.CASCADE)
-    order = models.IntegerField(_("Order"), default=0, db_index=True)
-    type = models.CharField(_("Type"), max_length=32)
-
-    def save(self, *args, **kwargs):
-        from django.db.models import F
-        if self.order is None or not self.order:
-            last_time = self.content.content_elements.order_by('-order').first()
-            self.order = last_time.order + 1
-        else:
-            conflicting_time = self.content.content_elements.filter(order=self.order).exists()
-            if conflicting_time:
-                self.timeline.time_links.filter(order__gte=self.order).update(order=F('order') + 1)
-        return super(ContentElement, self).save(*args, **kwargs)
