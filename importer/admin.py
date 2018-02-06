7e8bbe7f8e226ad31ff8a104169743fed376a685
Maaack <marek.belski@gmail.com>
2018-02-05 23:01:34 -0800

    Content Reading: Updates admin page with importer models

--- importer/admin.py
+++ importer/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import ImportedContent, ParseOperation, GroupMatch, TextMatch
+
+admin.site.register(ImportedContent)
+admin.site.register(ParseOperation)
+admin.site.register(GroupMatch)
+admin.site.register(TextMatch)
