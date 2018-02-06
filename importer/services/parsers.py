ee4e930423bee0c3ec9426ccc76b13d059b857fb
Maaack <marek.belski@gmail.com>
2018-02-05 23:15:00 -0800

    Content Reading: Updates SettingsRegexParser and adds CharacterRegexParser

--- importer/services/parsers.py
+++ importer/services/parsers.py
@@ -13,18 +13,34 @@ class RegexParser(ABC):
     def get_groups() -> tuple:
         pass
 
+    @staticmethod
+    def validate_result(result):
+        if type(result) is tuple:
+            return result
+        elif type(result) is dict:
+            return result
+        return None
+
+    @staticmethod
+    def clean_text(text):
+        return text.strip()
+
     def match_to_group(self, match):
         result = []
         groups = self.get_groups()
         for group in groups:
-            result.append((group[0], match.group(group[1])))
+            group_text = match.group(group[1])
+            if group_text:
+                clean_text = self.clean_text(group_text)
+                result.append((group[0], clean_text))
         return result
 
     def search(self, text: str):
         pattern = self.get_pattern()
         match = re.search(pattern, text)
         if match:
-            return self.match_to_group(match)
+            result = self.match_to_group(match)
+            return self.validate_result(result)
         return None
 
 
@@ -33,14 +49,49 @@ class SettingRegexParser(RegexParser):
     GROUP_LOCATION = 'location'
     GROUP_TIME = 'time'
 
+    GROUPS = (
+            (GROUP_POSITION, 1),
+            (GROUP_LOCATION, 3),
+            (GROUP_TIME, 5)
+        )
+
     @staticmethod
     def get_pattern():
-        return r"^(INT|EXT|EXT[\/\\]INT|INT[\/\\]EXT)[\s\.]+([\w\s']*)(-\s?([\w\s']*))?$"
+        return r"^((INT|EXT|EXT[\/\\]INT|INT[\/\\]EXT)\.+)([\w\s']*)(-\s?([\w\s']*))?$"
 
     @staticmethod
     def get_groups():
-        return (
-            (SettingRegexParser.GROUP_POSITION, 1),
-            (SettingRegexParser.GROUP_LOCATION, 2),
-            (SettingRegexParser.GROUP_TIME, 4)
+        return SettingRegexParser.GROUPS
+
+
+class CharacterRegexParser(RegexParser):
+    GROUP_FULL_TITLE = 'full_title'
+    GROUP_HONORIFIC = 'honorific'
+    GROUP_ROLE = 'role'
+    GROUP_NUMBER = 'number'
+    GROUP_POSITION = 'position'
+    GROUP_CONTINUED = 'continued'
+
+    GROUPS = (
+            (GROUP_FULL_TITLE, 1),
+            (GROUP_HONORIFIC, 2),
+            (GROUP_ROLE, 3),
+            (GROUP_NUMBER, 5),
+            (GROUP_POSITION, 6),
+            (GROUP_CONTINUED, 7)
         )
+
+    @staticmethod
+    def get_pattern():
+        return r"^(([A-Z]{2,4}\.)? ?([A-Z\d\-\. ]{3,40})(#(\d+))? ?)(\(V\.O\.\)|\(O\.S\.\))? ?(\(CONT'D\))?$"
+
+    @staticmethod
+    def get_groups():
+        return CharacterRegexParser.GROUPS
+
+    @staticmethod
+    def validate_result(result):
+        honorific = result[1][1]
+        if re.search(honorific, r"((INT|EXT|EXT[\/\\]INT|INT[\/\\]EXT)\.+)"):
+            return None
+        return RegexParser.validate_result(result)
