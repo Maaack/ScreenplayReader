bdc93e1f5198a248449975d338f2288a4816e5cd
Maaack <marek.belski@gmail.com>
2018-02-21 16:44:43 -0800

    Exporter: View for exporting scene-character breakdown

--- exporter/urls.py
+++ exporter/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.csv_preview, name='preview'),
     path('<int:screenplay_id>/', views.csv_export, name='detail'),
+    path('<int:screenplay_id>/characters', views.csv_export_character_breakout, name='character'),
 ]
--- exporter/views.py
+++ exporter/views.py
@@ -1,5 +1,6 @@
 import csv
 from django.http import HttpResponse
+from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
 from importer.models import Screenplay
 
@@ -32,4 +33,32 @@ def csv_export(request, screenplay_id):
         characters = ", ".join([character['title'] for character in scene.characters.values('title')])
         location = scene.location.title
         writer.writerow([scene_number, characters, location])
+    return response
+
+
+def csv_export_character_breakout(request, screenplay_id):
+    screenplay = get_object_or_404(Screenplay, pk=screenplay_id)
+    title = screenplay.title_pages.first().raw_title
+    # Create the HttpResponse object with the appropriate CSV header.
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="' + title + '- Scene Breakdown.csv"'
+    writer = csv.writer(response)
+
+    character_list = [character['title'] for character in
+                      screenplay.characters.annotate(line_count=Count('lines')).order_by('-line_count').values('title')]
+    headers = ['Number', 'Location']
+    headers += character_list
+    writer.writerow(headers)
+    scene_number = 0
+    for scene in screenplay.scenes.all():
+        scene_number += 1
+        scene_characters = [character['title'] for character in scene.characters.values('title')]
+        location = scene.location.title
+        row = [scene_number, location]
+        for character in character_list:
+            if scene_characters.count(character):
+                row.append('X')
+            else:
+                row.append('')
+        writer.writerow(row)
     return response
