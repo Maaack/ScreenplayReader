bd4d05e78c1409458fad7a3402fb5195cad06ba7
Maaack <marek.belski@gmail.com>
2018-03-12 20:47:37 -0700

    Split Interperter: Add another mixin for operation view sets

--- screenplayreader/mixins/views.py
+++ screenplayreader/mixins/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from rest_framework.renderers import JSONRenderer
 from rest_framework import status
+from rest_framework.decorators import detail_route
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
@@ -80,3 +81,14 @@ class BaseViewSet(ModelViewSet):
 
     class Meta:
         abstract = True
+
+
+class OperationViewSet(BaseViewSet):
+    class Meta:
+        abstract = True
+
+    @detail_route(methods=['post'],  url_path='run-operation')
+    def run_operation(self, request, pk = None):
+        operation = self.get_object()
+        operation.run_operation()
+        return Response('Operation complete!')
